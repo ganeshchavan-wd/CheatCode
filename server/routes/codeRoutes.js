ba7a82510dac8cfe808f8d2ae0abf0277a840432
udata_chinamyi09 <udatachinmayi@gmail.com>
2026-05-17 19:47:47 +0530

    Update codeRoutes.js

--- server/routes/codeRoutes.js
+++ server/routes/codeRoutes.js
@@ -3,7 +3,7 @@ import Code from '../models/Code.js'
 
 const router = express.Router()
 
-// Get all codes
+// GET all codes
 router.get('/', async (req, res) => {
   try {
     const codes = await Code.find()
@@ -13,54 +13,12 @@ router.get('/', async (req, res) => {
   }
 })
 
-// Add code
+// POST code
 router.post('/', async (req, res) => {
   try {
-    const { question, subject, code } = req.body
-
-    const newCode = new Code({
-      question,
-      subject,
-      code
-    })
-
+    const newCode = new Code(req.body)
     const savedCode = await newCode.save()
     res.status(201).json(savedCode)
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
-})
-
-// Get single code
-router.get('/:id', async (req, res) => {
-  try {
-    const code = await Code.findById(req.params.id)
-    res.json(code)
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
-})
-
-// Update code
-router.put('/:id', async (req, res) => {
-  try {
-    const updatedCode = await Code.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    )
-
-    res.json(updatedCode)
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
-})
-
-// Delete code
-router.delete('/:id', async (req, res) => {
-  try {
-    await Code.findByIdAndDelete(req.params.id)
-    res.json({ message: 'Code deleted' })
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
